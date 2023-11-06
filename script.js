072aa6f9ae6f5913e5caab2900738758b81abc4c
Fakhri <fakhrimg@code.edu.az>
2023-11-06 19:19:24 +0400

    script file was added

---
+++ script.js
@@ -0,0 +1,39 @@
+// TODO: Task-1:
+// let h1 = document.querySelector("h1");
+// console.log(h1);
+
+// TODO: Task-2:
+// document.body.removeChild(h1);
+// let pElement = document.createElement("p");
+// pElement.textContent = "TITAN RTX";
+// document.body.append(pElement);
+
+// TODO: Task-3:
+// let h1 = document.querySelector("h1");
+// h1.textContent = "GeForce 20 Series";
+// let liElement = document.querySelectorAll("li");
+
+// liElement.forEach((item) => {
+//   if (item.className == "primary") {
+//     item.style.backgroundColor = "#ebebeb";
+//   }
+// });
+
+// TODO: Task-4:
+// let liElement = document.querySelectorAll("li");
+
+// liElement.forEach((item) => {
+//   if (item.className == "primary") {
+//     console.log(item);
+//   }
+// });
+
+// TODO: Task-5:
+// let liElement = document.querySelectorAll("li");
+
+// liElement.forEach((item) => {
+//   item.classList.add("item");
+//   if (!item.classList.contains("primary")) {
+//     item.classList.add("secondary");
+//   }
+// });
